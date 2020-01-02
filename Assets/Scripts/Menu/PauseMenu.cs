78e811951f446949849f622bf5b854119b0f7af5
Borisov Dmitry <deborisov_1@edu.hse.ru>
2020-01-02 22:29:42 +0300

    Pause manu initial

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsOnPause = false;
+    public GameObject pauseUI;
+    public GameObject pauseBtn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("LOL");
+            if (!GameIsOnPause)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsOnPause = false;
+        if (pauseBtn != null)
+        {
+            pauseBtn.SetActive(true);
+        }
+    }
+
+    public void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsOnPause = true;
+        if (pauseBtn != null)
+        {
+            pauseBtn.SetActive(false);
+        }
+    }
+
+    public void GoToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+}
