3b372e292281e80cd8fc861883626acf4de7b0bd
Borisov Dmitry <deborisov_1@edu.hse.ru>
2020-01-09 21:22:21 +0300

    Added joystick to 2dPong

--- Assets/Scripts/2dPong/PaddleScript.cs
+++ Assets/Scripts/2dPong/PaddleScript.cs
@@ -8,6 +8,8 @@ public class PaddleScript : MonoBehaviour
     private Vector2 screenBounds;
     private float objectWidth;
     private float objectHeight;
+    public GameObject ball;
+    public float speed;
 
     void Start()
     {
@@ -16,7 +18,7 @@ public class PaddleScript : MonoBehaviour
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         if (isRight)
         {
-            transform.position = new Vector2 (screenBounds.x - objectWidth * 2, 0);
+            transform.position = new Vector2(screenBounds.x - objectWidth * 2, 0);
         }
         else
         {
@@ -24,9 +26,47 @@ public class PaddleScript : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+    private void OnEnable()
+    {
+        if (!isRight)
+        {
+            Joystick.OnMakeMove += MakeMove;
+        }
+    }
+
+    private void OnDisable()
+    {
+        Joystick.OnMakeMove -= MakeMove;
+    }
+
+    void MakeMove(Vector2 dir)
+    {
+        Vector2 direction = new Vector2(dir.y, 0);
+        Debug.Log(direction.ToString());
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
     void Update()
     {
-        
+        if (isRight)
+        {
+            AIForRight();
+        }
+    }
+
+    void AIForRight()
+    {
+        Vector3 vel = ball.GetComponent<Rigidbody2D>().velocity;
+        if (vel.x > 0)
+        {
+            if (ball.transform.position.y > transform.position.y)
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+            }
+            else
+            {
+                transform.Translate(Vector2.left * Time.deltaTime * speed);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/General/Joystick.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    public Transform player;
+    public float speed;
+    private bool touchStart = false;
+    private Vector2 startPoint, endPoint;
+    public Transform innerCircle;
+    public Transform outerCircle;
+    private Vector2 direction;
+    public Vector2 Direction
+    {
+        get
+        {
+            return direction;
+        }
+    }
+    public delegate void MakeMove(Vector2 d);
+    public static event MakeMove OnMakeMove;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+            innerCircle.position = startPoint;
+            outerCircle.position = startPoint;
+            innerCircle.GetComponent<SpriteRenderer>().enabled = true;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = true;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            touchStart = true;
+            endPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else
+        {
+            touchStart = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector2 offset = endPoint - startPoint;
+            direction = Vector2.ClampMagnitude(offset, 1.0f);
+            innerCircle.position = new Vector2(startPoint.x + direction.x, startPoint.y + direction.y);
+            OnMakeMove(direction);
+        }
+        else
+        {
+            innerCircle.GetComponent<SpriteRenderer>().enabled = false;
+            outerCircle.GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+}
